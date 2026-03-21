324d881c2d5b49201e9b38071a097d8ebd30524d
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2026-03-21 17:56:32 -0400

    fix: update vercel to work everytime

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/data/:match*",
+        destination: "https://www.bub.icu/_vercel/insights/:match*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
-import { Analytics } from "@vercel/analytics/next";
+import { VercelAnalytics } from "@/lib/analytics";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({
@@ -31,7 +31,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
         {children}
         <ReactQueryDevtools initialIsOpen={false} />
         <Toaster position="top-right" richColors />
-        <Analytics />
+        <VercelAnalytics />
       </QueryClientProvider>
     </SessionProvider>
   );
